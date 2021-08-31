fdca83516635979a1c643b0b95dc9a6e57ada4cf
Sibel Eren <edaeren575@gmail.com>
2021-08-31 14:11:00 +0100

    can deposit and withdraw from account

---
+++ lib/bank_account.rb
@@ -0,0 +1,19 @@
+class BankAccount
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def deposit(money)
+    @balance += money
+  end
+
+  def withdraw(money)
+    @balance -= money
+  end
+
+
+
+end
